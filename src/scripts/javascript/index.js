56be202733ffb63f8d0906bad45361f10a8b980f
Cledeocir Marafão <cledeocirms96@gmail.com>
2025-11-04 07:34:23 -0300

    feat(index.js): Adicionado o arquivo javascript

--- src/scripts/javascript/index.js
+++ src/scripts/javascript/index.js
@@ -1,10 +1,55 @@
-import { menu } from "./utils/navbar.js"
+import { menu } from "./utils/navbar.js";
+menu();
 
-async function url() {
-    const response = await fetch('https://loteriascaixa-api.herokuapp.com/v2/api-docs')
-    const json = await response.json()
-    
+const BASE_API = "https://loteriascaixa-api.herokuapp.com/api";
+
+function init() {
+  getLotery();
+  getMegaSena();
+
+  if (window.location.pathname.endsWith('index.html') || window.location.pathname === '/') {
+
+    setDataContest("megasena");
+  setDataContest("quina");
+  setDataContest("lotofacil");
+  setDataContest("lotomania");
+  setDataContest("timemania");
+  setDataContest("duplasena");
+  }
+  
+  
 }
-url()
+init();
 
-menu()
+async function getLotery() {
+  try {
+    const response = await fetch(`${BASE_API}`);
+    const data = await response.json();
+  } catch (error) {
+    console.log("Erro ao buscar os jogos:", error);
+  }
+}
+
+async function getMegaSena() {
+  try {
+    const response = await fetch(`${BASE_API}/megasena/`);
+    const data = await response.json();
+  } catch (error) {
+    console.log("Erro ao buscar o jogo");
+  }
+}
+
+async function setDataContest(gameName) {
+  try {
+    const response = await fetch(`${BASE_API}/${gameName}/latest`);
+    const data = await response.json();
+
+    const element = document.getElementById(`next-${gameName}`);   
+
+    element.textContent = data.dataProximoConcurso || "Data indisponível";
+  } catch (error) {
+    console.error(`Erro ao carregar dados da ${gameName}:`, error);
+  }
+}
+
+export { getLotery };
