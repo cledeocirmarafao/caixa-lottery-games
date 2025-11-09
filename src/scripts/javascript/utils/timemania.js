68bc3e6368e812d8d7e800003bc004696ab90173
Cledeocir Marafão <cledeocirms96@gmail.com>
2025-11-09 13:15:38 -0300

    feat: Adicionada as funções  específicas desta página

---
+++ src/scripts/javascript/utils/timemania.js
@@ -0,0 +1,21 @@
+const BASE_API = "https://loteriascaixa-api.herokuapp.com/api";
+
+async function getTimemania() {
+  const response = await fetch(`${BASE_API}/timemania`);
+  const data = await response.json();
+
+  let teamFilter = {
+    team: data[0].timeCoracao,
+  };
+
+  document.getElementById("team-draw").textContent = teamFilter.team;
+
+  const results = document.querySelectorAll(".previous-draw-team");
+
+  results.forEach((block, i) => {
+    if (block) {
+      block.textContent = teamFilter.team;
+    }
+  });
+}
+getTimemania();
