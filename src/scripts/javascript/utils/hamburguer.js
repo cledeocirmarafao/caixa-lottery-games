1307b2d528cb50496e8db1dee0066bfdf8bed6b6
Cledeocir Marafão <cledeocirms96@gmail.com>
2025-11-12 09:44:43 -0300

    feat: configurações do menu hamburguer

---
+++ src/scripts/javascript/utils/hamburguer.js
@@ -0,0 +1,27 @@
+function hamburguer() {
+  const toggleBtn = document.querySelector(".navbar-toggle");  
+  const menu = document.getElementById("navbar-menu");
+
+  const links = document.querySelectorAll(".navbar-link");
+
+  toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      toggleBtn.classList.remove("active");
+      menu.classList.remove("active");
+    });
+  });
+
+    document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target) && !toggleBtn.contains(e.target)) {
+      toggleBtn.classList.remove("active");
+      menu.classList.remove("active");
+    }
+  });
+}
+
+export { hamburguer };
